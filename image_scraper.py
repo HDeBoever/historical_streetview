fd387c689c0f1550ebcfc315b43c9c4dc884d19d
HDeBoever <henrideboever@gmail.com>
2020-04-28 11:56:45 -0700

    Working example of image scraper for given URL

--- image_scraper.py
+++ image_scraper.py
@@ -1,45 +1,41 @@
-import re, sys, io, requests
-import urllib.request
-from PIL import Image
 from bs4 import BeautifulSoup
+import urllib.request
+import shutil
+import requests
+from urllib.parse import urljoin
+import sys
+import time
 
+def make_soup(url):
+	req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
+	html = urllib.request.urlopen(req)
+	return BeautifulSoup(html, 'html.parser')
 
-def scrape_images_from_given_url(url):
-
-	site = url
-
-	response = requests.get(site)
-
-	soup = BeautifulSoup(response.text, 'html.parser')
-	img_tags = soup.find_all('img')
-
-	urls = [img['src'] for img in img_tags]
-
-	print(urls)
-
-	for url in urls:
+def get_images(url):
+	soup = make_soup(url)
+	images = [img for img in soup.findAll('img')]
+	print (str(len(images)) + " images found.")
+	print('Downloading images to current working directory.')
+	image_links = [each.get('src') for each in images]
+	for each in image_links:
+		print(each)
 		try:
-			filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
-			if 'http' not in url:
-				# sometimes an image source can be relative
-				# if it is provide the base url which also happens
-				# to be the site variable atm.
-				url = '{}{}'.format(site, url)
-
-			print(url, filename.group(1))
-			print('\n')
-			print(filename)
-			print('\n')
-			print(filename.group(1))
-			sys.exit(0)
-			urllib.request.urlretrieve(url, filename.group(1))
-		except AttributeError as e:
-			print(e)
+			filename = each.strip().split('/')[-1].strip()
+			src = urljoin(url, each)
+			print('Getting: ' + filename)
+			response = requests.get(src, stream=True)
+			# delay to avoid corrupted previews
+			#time.sleep(1)
+			with open(filename, 'wb') as out_file:
+				shutil.copyfileobj(response.raw, out_file)
+		except:
+			print('An error occured. Continuing.')
+	print('Done.')
 
 def main(argv):
 
-	# Attempt to scrape images from a given URL; save the images to the curr directory
-	scrape_images_from_given_url('https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal')
+	# Scrape images from a given URL; save the images to the curr directory
+	get_images('https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main(sys.argv)
