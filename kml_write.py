c0fcef9bb2552b352c9c7ac0a90f525d16ffbc97
HDeBoever <henrideboever@gmail.com>
2020-05-06 10:52:57 -0700

    opens and creates skeleton .kml file

--- kml_write.py
+++ kml_write.py
@@ -13,14 +13,10 @@ def main(argv):
 
 	relative_path = path + "/test_kml_files"
 	kml = simplekml.Kml()
-	file = (relative_path + '/' + 'test_kml.txt')
-	# Check if the file exists
-	if os.path.isfile(file):
-		with open(file, 'r') as f:
-			# Check if the file was empty before making a point object
-			if(os.stat(file).st_size > 0):
-				point = kml.newpoint(name = ("test point"), coords = [0, 0, 0])
-				kml.save(relative_path + '/' + file)
+	file = ('test_kml.txt')
+	f = open(file, "w")
+	point = kml.newpoint(name = ("test point"), coords = [(0, 0, 0)])
+	kml.save(relative_path + '/' + file)
 
 if __name__ == '__main__':
 	main(sys.argv)
