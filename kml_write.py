3493e925d10961564dfc8e165b07ec9d49731a89
HDeBoever <henrideboever@gmail.com>
2020-05-08 13:04:36 -0700

    Interface avec API de Google Places

--- kml_write.py
+++ kml_write.py
@@ -1,7 +1,31 @@
 import simplekml, os, sys
+# import googlemaps
+from datetime import datetime
+from googleplaces import GooglePlaces, types, lang
 
 
-def main(argv):
+# function to test the access to a google API and the place attributes
+def test_google_api():
+
+	f = open("places_api_key.txt", "r")
+	api_key = f.read()
+	google_places = GooglePlaces(api_key)
+
+	query_result = google_places.nearby_search(
+		lat_lng={'lat': 46.1667, 'lng': -1.15},
+		radius=5000,
+		types=[types.TYPE_RESTAURANT] or [types.TYPE_CAFE] or [type.TYPE_BAR] or [type.TYPE_CASINO])
+
+	if query_result.has_attributions:
+		print(query_result.html_attributions)
+
+	for place in query_result.places:
+		place.get_details()
+		# print(dir(place))
+		print(place.rating)
+		print(place.name)
+
+def write_to_kml():
 
 	# simplekml takes coords in long/lat format
 	path = os.getcwd()
@@ -17,6 +41,14 @@ def main(argv):
 	f = open(file, "w")
 	point = kml.newpoint(name = ("test point"), coords = [(0, 0, 0)])
 	kml.save(relative_path + '/' + file)
+
+
+def main(argv):
+
+	# Main is used for testing purposes
+	test_google_api()
+
+
 
 if __name__ == '__main__':
 	main(sys.argv)
